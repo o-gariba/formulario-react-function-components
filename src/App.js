d10db68e8a3d8e8f0c3f6aeaad62bb138bd1b9c3
Pedro Garibaldi Santos <pedrogaribaldisantos@gmail.com>
2022-04-13 18:57:01 -0300

    Material UI em 100% da interface

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import { Component, Fragment } from 'react';
+import { Component } from 'react';
 import './App.css';
 import FormCadastro from './components/FormCadastro/FormCadastro';
+import { Container, Typography } from '@mui/material'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 class App extends Component{
   render() {
     return (
-      // Fragment encapsula todas as tags para retornarmos apenas uma tag mãe, e não é renderizado no HTML.
-      <Fragment>
-        <h1>Formulário de cadastro</h1>
+      // <Fragment> encapsula todas as tags para retornarmos apenas uma tag mãe, e não é renderizado no HTML.
+      // Usando o Material UI, vamos usar o <article> ao invés do Fragment, pois ele já pode ter estilos próprios. article por definição é um elemento auto contido dentro do site, que eu possa mudar ele de página sem perder informações  
+      <Container maxWidth="sm" component="article">
+        <Typography variant='h4' align='center' component="h1">Formulário de cadastro</Typography>
         <FormCadastro/>
-      </Fragment>
+      </Container>
     );
   }
 }
--- src/components/FormCadastro/FormCadastro.jsx
+++ src/components/FormCadastro/FormCadastro.jsx
@@ -1,24 +1,21 @@
 import React from "react";
-import { Button } from "@mui/material"
+import { Button, FormControlLabel, Switch, TextField } from "@mui/material"
 
 // Function components é uma abordagem mais moderna, não necessáriamente melhor. Acho q é mais rápida de ser criada e usada para quando estamos lidando com stateless components (não muda o estado, como um componente que atualiza um bloco de notas)
 function FormCadastro() {
     return(
         <form>
-            <label>Nome Completo</label>
-            <input type="text"/>
+            <TextField id="nome" label="Nome Completo" variant="standard" margin="dense" fullWidth required/>
 
-            <label>CPF</label>
-            <input type="text"/>
+            <TextField id="cpf" label="CPF" variant="standard" margin="dense" fullWidth required/>
             
-            <label>E-mail</label>
-            <input type="email"/>
+            <TextField id="email" label="E-mail" variant="standard" margin="dense" fullWidth required/>
 
-            <label>promoções</label>
-            <input type="checkbox"/>
+            {/* Existe o checkbox no Material UI mas hj está mais em moda usar o INTERRUPTOR (switch) para a mesma finalidade */}
 
-            <label>promoções</label>
-            <input type="checkbox"/>
+            <FormControlLabel label="Promoções" control={<Switch name="promocoes" color="primary" defaultChecked />}/>
+
+            <FormControlLabel label="Novidades" control={<Switch name="novidades" color="primary" defaultChecked />}/>
 
             <Button variant="contained" color="primary" type="submit">Cadastrar</Button>
         </form>
